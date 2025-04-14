f1d4b185d10be66755c310f930547b436430db15
YudaiUrabe <yudaiurabe@gmail.com>
2025-04-14 15:42:08 +0900

    initial

---
+++ cekaddmul.ml
@@ -0,0 +1,46 @@
+
+module StringMap = Map.Make(String)
+
+(* Object Langugae *)
+
+
+
+
+(* CEK Machine *)
+(* SYNTAX of CEK machine *)
+
+(* state
+ triple of an expression, an environment and continuation
+*)
+
+
+
+
+
+(* SEMANTICS of CEK machine *)
+
+(* injection function  *)
+
+
+
+(* (one-step) transition function *)
+
+
+
+
+
+
+(* evaluation function *)
+
+
+
+
+
+
+(* test *)
+ let term_test = 
+
+let result = evaluate term_test
+
+
+
