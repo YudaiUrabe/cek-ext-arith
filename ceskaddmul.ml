eb2c87cf045bdc965de91aea00cfdcae2fa199a0
YudaiUrabe <yudaiurabe@gmail.com>
2025-05-05 19:43:45 +0900

    initial commit

---
+++ ceskaddmul.ml
@@ -0,0 +1,33 @@
+
+
+(* Object Langugae *)
+
+
+
+
+
+(* CEK Machine *)
+(* SYNTAX of CEK machine *)
+
+(* state *)
+
+
+(* syntactic sugar *)
+
+
+
+(* SEMANTICS of CEK machine *)
+
+(* injection function  *)
+
+(* (one-step) transition function *)
+
+
+(* auxiliary functions for evaluation function *)
+(* isFinal *)
+
+(* evaluation function *)
+
+
+(* test *)
+
